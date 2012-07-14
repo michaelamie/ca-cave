0e18c3491bbcb0479ba4ac5d5a39b3832b1e16ef
Michael Amie <michael.amie@gmail.com>
2012-07-13 21:20:57 -0700

    Tidying up

--- ca-cave.py
+++ ca-cave.py
@@ -9,26 +9,26 @@ def drawMap(screen, map, rows, cols):
       if map[row][col] == True:
         screen.addch(row, col, '#')
 
-def countNeighbors(map, row, col):
+def countNeighbors(map, rows, cols, row, col):
   count = 0
   # Left column
   if row-1 >= 0 and col-1 >= 0 and map[row-1][col-1] == True:
     count += 1
   if col-1 >= 0 and map[row][col-1] == True:
     count += 1
-  if row+1 < len(ROWS) and col-1 >= 0 and map[row+1][col-1] == True:
+  if row+1 < len(rows) and col-1 >= 0 and map[row+1][col-1] == True:
     count += 1
   # Center column
   if row-1 >= 0 and map[row-1][col] == True:
     count += 1
-  if row+1 < len(ROWS) and map[row+1][col] == True:
+  if row+1 < len(rows) and map[row+1][col] == True:
     count += 1
   # Right column
-  if row-1 >= 0 and col+1 < len(COLS) and map[row-1][col+1] == True:
+  if row-1 >= 0 and col+1 < len(cols) and map[row-1][col+1] == True:
     count += 1
-  if col+1 < len(COLS) and map[row][col+1] == True:
+  if col+1 < len(cols) and map[row][col+1] == True:
     count += 1
-  if row+1 < len(ROWS) and col+1 < len(COLS) and map[row+1][col+1] == True:
+  if row+1 < len(rows) and col+1 < len(cols) and map[row+1][col+1] == True:
     count += 1
   return count
 
@@ -42,27 +42,27 @@ try:
   curses.curs_set(0)
 
   # Values
-  ROWS = range(23)
-  COLS = range(80)
+  rows = range(23)
+  cols = range(80)
 
   # Populate and randomize map
-  map = [[random.randint(1, 100) < 36 for col in COLS] for row in ROWS]
+  map = [[random.randint(1, 100) < 36 for col in cols] for row in rows]
 
   # Fill in edges
-  for row in ROWS:
+  for row in rows:
     map[row][0] = True
     map[row][1] = True
-    map[row][len(COLS)-1] = True
-    map[row][len(COLS)-2] = True
-  for col in COLS:
+    map[row][len(cols)-1] = True
+    map[row][len(cols)-2] = True
+  for col in cols:
     map[0][col] = True
     map[1][col] = True
-    map[len(ROWS)-1][col] = True
-    map[len(ROWS)-2][col] = True
+    map[len(rows)-1][col] = True
+    map[len(rows)-2][col] = True
 
   # Draw map before iterations
-  drawMap(screen, map, ROWS, COLS)
-  screen.addstr(len(ROWS), 0, "Initial state")
+  drawMap(screen, map, rows, cols)
+  screen.addstr(len(rows), 0, "Initial state")
   screen.refresh()
   time.sleep(.1)
 
@@ -71,9 +71,9 @@ try:
   while True:
     iteration += 1
     changed = False
-    for row in ROWS:
-      for col in COLS:
-        count = countNeighbors(map, row, col)
+    for row in rows:
+      for col in cols:
+        count = countNeighbors(map, rows, cols, row, col)
         # Modify the current cell
         if map[row][col] == False and count >= 5:
           map[row][col] = True
@@ -83,8 +83,8 @@ try:
           changed = True
 
     # Draw map for each iteration
-    drawMap(screen, map, ROWS, COLS)
-    screen.addstr(len(ROWS), 0, "CA iteration: %d" % iteration)
+    drawMap(screen, map, rows, cols)
+    screen.addstr(len(rows), 0, "CA iteration: %d" % iteration)
     screen.refresh()
     time.sleep(.1)
 
