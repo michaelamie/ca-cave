cf8690e4e63d5444b394944233910d4fa648f9dd
Michael Amie <michael.amie@gmail.com>
2012-08-22 22:23:17 -0700

    Modified ca algorithm so that the map isn't updated until the end of each pass and it follows the "4-5 rule."

--- ca-cave.py
+++ ca-cave.py
@@ -15,9 +15,15 @@ def countNeighbors(map, row, col):
     for x in [col-1, col, col+1]:
       if map[y][x]:
         count += 1
-  if map[row][col]:
-    count -= 1
   return count
+
+def addBorders(map, rows, cols):
+  for row in rows:
+    for col in [0, len(cols)-1]:
+      map[row][col] = True
+  for col in cols:
+    for row in [0, len(rows)-1]:
+      map[row][col] = True
 
 
 ################################################################################
@@ -32,46 +38,48 @@ try:
   # Values
   rows = range(23)
   cols = range(80)
+  delay = .1
 
   # Populate and randomize map
-  map = [[random.randint(1, 100) < 36 for col in cols]
+  map = [[random.randint(1, 100) < 40 for col in cols]
          for row in rows]
-
-  # Populate borders with walls
-  for row in rows:
-    for col in [0, len(cols)-1]:
-      map[row][col] = True
-  for col in cols:
-    for row in [0, len(rows)-1]:
-      map[row][col] = True
+  addBorders(map, rows, cols)
 
   # Draw map before iterations
   drawMap(screen, map, rows, cols)
   screen.addstr(len(rows), 0, "Initial state")
   screen.refresh()
-  time.sleep(.1)
+  time.sleep(delay)
 
   # Iterate cellular automata algorithm
   iteration = 0
   while True:
     iteration += 1
     changed = False
+
+    # Initialize next map
+    nextMap = [[False for col in cols] for row in rows]
+    addBorders(nextMap, rows, cols)
+
+    # Count the neighbors of each cell and populate next map
     for row in rows[1:len(rows)-1]:
       for col in cols[1:len(cols)-1]:
         count = countNeighbors(map, row, col)
         # Modify the current cell
+        if map[row][col] == True and count >= 4:
+          nextMap[row][col] = True
         if map[row][col] == False and count >= 5:
-          map[row][col] = True
+          nextMap[row][col] = True
           changed = True
-        if map[row][col] == True and count <= 2:
-          map[row][col] = False
-          changed = True
+
+    # Update map
+    map = nextMap
 
     # Draw map for each iteration
     drawMap(screen, map, rows, cols)
     screen.addstr(len(rows), 0, "CA iteration: %d" % iteration)
     screen.refresh()
-    time.sleep(.1)
+    time.sleep(delay)
 
     # Break out of the loop if we've reached equilibrium
     if not changed:
